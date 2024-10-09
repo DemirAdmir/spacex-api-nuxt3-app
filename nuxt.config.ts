8c6e13e073b79d8de55a33beb8c51843b8e104f7
admirdemir <admirdemir@gmail.com>
2024-10-09 22:04:58 +0200

    Configure Nuxt 3 and add environment variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@pinia/nuxt"],
+  typescript: {
+    strict: true,
+  },
+  runtimeConfig: {
+    mongoUri: process.env.MONGODB_URI,
+  },
+});
